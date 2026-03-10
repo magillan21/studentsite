d102eb411adaf3da5f9b81a794d697b313e129a8
magillan21 <magillan@hawaii.edu>
2026-03-10 10:28:58 -1000

    toggle nav bar

--- art128/base-structure/js/script.js
+++ art128/base-structure/js/script.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    //JAVASCRIPT FUNCTIONS GO HERE
-    console.log("JavaScript is connected and running!");
+    //TOGGLE NAV
+    const navButton = document.querySelector('.menu-button');
+    const navList = document.querySelector('.site-nav');
+
+    // Check Nav State 
+    navButton.onclick = () => {
+        if (navList.getAttribute('data-nav-state') === 'closed') {
+            navList.setAttribute('data-nav-state', 'open');
+        } else {
+            navList.setAttribute('data-nav-state', 'closed');
+        }
+
+    }
 });
